d359de9027df1bbf5f02ea4ecbd961088c3bc114
Mateusz <mateusz.ruszczynski@gmail.com>
2025-11-24 14:39:29 +0100

    Solution for shop-trip

--- app/shop.py
+++ app/shop.py
@@ -18,7 +18,7 @@ class Shop:
         return total_price
 
     def print_receipt(self, customer: "Customer", cart_item: dict) -> None:
-        print(f"Date: {datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}")
+        print(f"Date: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
         print(f"Thanks, {customer.name}, for your purchase!")
         print("You have bought:")
 
